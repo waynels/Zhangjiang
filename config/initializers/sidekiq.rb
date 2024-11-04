b149acde823e805b7044377bc720798bb768409a
clover <clover_0201@163.com>
2024-11-04 18:21:10 +0800

    change: sidekiq db

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,10 @@
 Sidekiq.configure_server do |config|
-  config.redis = { host: ENV['REDIS_HOST'], port: '6379', db: 10 }
+  config.redis = { host: ENV['REDIS_HOST'], port: '6379', db: 5 }
   # config.server_middleware do |chain|
   #   chain.add Sidekiq::JobRetry, max_retries: 1
   # end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { host: ENV['REDIS_HOST'], port: '6379', db: 10 }
+  config.redis = { host: ENV['REDIS_HOST'], port: '6379', db: 5 }
 end
