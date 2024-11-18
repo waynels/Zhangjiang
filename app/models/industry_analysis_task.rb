215a965d280e7bc27f5b1fd9ce07f8b9cc5b6965
clover <clover_0201@163.com>
2024-11-18 14:04:49 +0800

    fix: industry_analysis_tasks

--- app/models/industry_analysis_task.rb
+++ app/models/industry_analysis_task.rb
@@ -21,24 +21,33 @@ class IndustryAnalysisTask < ApplicationRecord
   end
 
   def key_enterprise_talent_json_data
-    fields = ::EnterpriseFieldRecord.where(batch: batch)
-    record_ids = ::TalentFieldRecord.where(enterprise_record_id: fields.pluck(:record_id)).pluck(:record_id)
-    update(record_ids: record_ids, table_id: 'tbleTvx12bkJHzWa')
-    { "sector": "人工智能", "batch": batch, "strategy": 2, "enterprises": fields.map(&:talents_info) }
+    fields = ::TalentFieldRecord.where(batch: batch)
+    enterprises = fields.group_by(:enterprise_record_id).map do |enterprise_id, items|
+      enterprise = ::EnterpriseFieldRecord.find_by(record_id: enterprise_id)
+      { code: enterprise.code, name: enterprise.name, talents: items.map(&base_fields)}
+    end
+    update(record_ids: fields.pluck(:record_id), table_id: 'tbleTvx12bkJHzWa')
+    { "sector": "人工智能", "batch": batch, "strategy": 2, "enterprises": enterprises }
   end
 
   def key_enterprise_financing_json_data
-    fields = ::EnterpriseFieldRecord.where(batch: batch)
-    record_ids = ::RoundFieldRecord.where(enterprise_record_id: fields.pluck(:record_id)).pluck(:record_id)
-    update(record_ids: record_ids, table_id: 'tbln8iAx3QRZZeUM')
-    { "sector": "人工智能", "batch": batch, "strategy": 2, "enterprises": fields.map(&:rounds_info) }
+    fields = ::RoundFieldRecord.where(batch: batch)
+    enterprises = fields.group_by(:enterprise_record_id).map do |enterprise_id, items|
+      enterprise = ::EnterpriseFieldRecord.find_by(record_id: enterprise_id)
+      { code: enterprise.code, name: enterprise.name, rounds: items.map(&base_fields)}
+    end
+    update(record_ids: fields.pluck(:record_id), table_id: 'tbln8iAx3QRZZeUM')
+    { "sector": "人工智能", "batch": batch, "strategy": 2, "enterprises": enterprises }
   end
 
   def key_enterprise_product_json_data
-    fields = ::EnterpriseFieldRecord.where(batch: batch)
-    record_ids = ::ProductFieldRecord.where(enterprise_record_id: fields.pluck(:record_id)).pluck(:record_id)
-    update(record_ids: record_ids, table_id: 'tblzWemRbHEXpIoj')
-    { "sector": "人工智能", "batch": batch, "strategy": 2, "enterprises": fields.map(&:products_info) }
+    fields = ::ProductFieldRecord.where(batch: batch)
+    enterprises = fields.group_by(:enterprise_record_id).map do |enterprise_id, items|
+      enterprise = ::EnterpriseFieldRecord.find_by(record_id: enterprise_id)
+      { code: enterprise.code, name: enterprise.name, products: items.map(&base_fields)}
+    end
+    update(record_ids: fields.pluck(:record_id), table_id: 'tblzWemRbHEXpIoj')
+    { "sector": "人工智能", "batch": batch, "strategy": 2, "enterprises": enterprises }
   end
 
   def trends_json_data
@@ -54,8 +63,7 @@ class IndustryAnalysisTask < ApplicationRecord
   end
 
   def data_map_json_data
-    enterprise_ids = ::EnterpriseFieldRecord.where(batch: batch).pluck(:record_id)
-    fields = ::GraphFieldRecord.where(enterprise_record_id: enterprise_ids)
+    fields = ::GraphFieldRecord.where(batch: batch)
     update(record_ids: fields.pluck(:record_id), table_id: 'tbltzsmAUdkoSJk7')
     types = fields.group_by(&:level1).map do |level, items|
       {"label": level, "children": items.group_by(&:level2).map {|level2, enterprises| { "label": level2, "enterprises": enterprises.map(&:base_fields) }}}
