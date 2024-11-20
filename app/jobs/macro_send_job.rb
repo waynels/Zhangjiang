90ef289444131de54fa8f862eaca85ac9968bbd4
clover <clover_0201@163.com>
2024-11-20 20:15:17 +0800

    fix

--- app/jobs/macro_send_job.rb
+++ app/jobs/macro_send_job.rb
@@ -4,7 +4,7 @@ class MacroSendJob < ApplicationJob
   def perform(record_id)
     record = ::MacroFieldRecord.find(record_id)
     server = ::ZhangJiangService.new(ENV['ZHANGJIANG_USERID'], ENV['ZHANGJIANG_SECRET'])
-    result = server.macro(id)
+    result = server.macro(record.id)
     if result.present?
       record.update(acknowledgment: result['data']['acknowledgment'], batch_updated_at: Time.new)
     end
